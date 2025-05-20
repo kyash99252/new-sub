27069a5a2b864410a6a3b409b8f801c4b42e9e33
Ruchi <ruchiingale079@gmail.com>
2025-05-20 20:09:26 +0530

    first commit

---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+// app/api/auth/[...nextauth]/route.ts
+
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+})
